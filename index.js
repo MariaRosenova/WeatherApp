b15d33e194b71026440cfc69b85eea2bb35b44c4
Maria <mimito1353@gmail.com>
2024-01-09 09:29:03 +0200

    Change the html structure

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// const searchSide = document.getElementsByClassName("search-bar")[0];
-
-// window.addEventListener("scroll", function() {
-
-//     var scrollX = window.scrollX;
-//     var scrollY = window.scrollY;
-
-//     console.log(`Y= ${scrollY}`)
-//     //750
-  
-//     let currentWidth = 100 - scrollY / 100 * 11;
-//     console.log(currentWidth)
-//     searchSide.style.width = `${currentWidth}%`;
-//     searchSide.style.opacity = currentWidth / 100;
-
-// });
-
 const searchSide = document.getElementsByClassName("search-bar")[0];
 
 window.addEventListener("DOMContentLoaded", function() {
