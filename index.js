de1dea93ed3964414f874b64ca9965ad158b511f
Maria <mimito1353@gmail.com>
2024-01-22 11:03:36 +0200

    Add automatic scroll function when the result is fetched.

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+let flag = false;
 const searchSide = document.getElementsByClassName("search-bar")[0];
 
-window.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-        searchSide.style.opacity = 1 - this.window.scrollY / window.innerHeight;
-        searchSide.style.backgroundColor = "black";
-       
-    });
-   
+window.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    searchSide.style.opacity = 1 - this.window.scrollY / window.innerHeight;
+    searchSide.style.backgroundColor = "black";
+  });
 });
 
 const searchBtn = document.querySelector("#search-btn");
 
-searchBtn.addEventListener('click', () => {
-    //const input = document.querySelector("#input-bar").value;
-   
-    const apiKey = "cbb16815a867f2287841ffe07679f415";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=sofia";
+searchBtn.addEventListener("click", () => {
+  const searchCity = document.querySelector("#input-bar").value;
 
-    async function checkWeather() {
-        const response = await fetch(apiUrl + `&apiKey=${apiKey}`);
-        var data = await response.json();
-        console.log(data);
-    }
+  const apiKey = "cbb16815a867f2287841ffe07679f415";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-    checkWeather();
+  async function checkWeather() {
+    const response = await fetch(
+      apiUrl + `&apiKey=${apiKey}` + `&q=${searchCity}`
+    );
+    var data = await response.json();
+    flag = true;
+    console.log(data);
+  }
+
+  if (!flag) {
+    alert("Error! Please, type correct information!");
+  }
+  scrollResultsIntoView();
+
+  checkWeather();
 });
 
+function scrollResultsIntoView() {
+  const results = document.querySelector("main");
+  results.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
