e9dfc097b79ffdf1e2103c83f295422e265f000c
Maria <mimito1353@gmail.com>
2024-01-22 10:44:37 +0200

    add an API

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ window.addEventListener("DOMContentLoaded", function() {
     });
    
 });
+
+const searchBtn = document.querySelector("#search-btn");
+
+searchBtn.addEventListener('click', () => {
+    //const input = document.querySelector("#input-bar").value;
+   
+    const apiKey = "cbb16815a867f2287841ffe07679f415";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=sofia";
+
+    async function checkWeather() {
+        const response = await fetch(apiUrl + `&apiKey=${apiKey}`);
+        var data = await response.json();
+        console.log(data);
+    }
+
+    checkWeather();
+});
+
